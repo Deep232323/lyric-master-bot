d8ee771ddf9ced84e320cebf2c557caeb5f4e82f
Deep232323 <71260317+Deep232323@users.noreply.github.com>
2020-09-14 15:35:48 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.NzEzNjM0NDY5Nzg1MTA4NTAx.Xsi95A.p4yVUpnOXM0SevL_RqwN4iokI_A);//BOT_TOKEN is the Client Secret
